a74e37655cbcf41ae9e399761ad0e17d6f176d5e
fsce1 <jamieserlin@gmail.com>
2022-11-28 12:18:21 +0000

    blender VHS nodes n dat

---
+++ Blender/Tex/Comp/interlace-video.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+# Forces interlacing of a series of frames into a lossless mp4 file.
+
+if [ "$1" = "-h" ]; then
+   echo "Forces interlacing of a series of frames into a lossless x264 encoded mp4 file."
+   echo "interlace-video.sh -i [input=render/%4d.png] -o [output=render/interlaced.mp4] -r [rate=30]"
+   exit
+fi
+
+input="render/%4d.png"
+output="render/interlaced.mp4"
+rate="30"
+
+while getopts ":i:o:r:" opt; do
+   case $opt in
+      i) input="$OPTARG"
+      ;;
+      o) output="$OPTARG"
+      ;;
+      r) rate="$OPTARG"
+      ;;
+      \?) echo "Invalid option -$OPTARG" >&2
+	  exit
+      ;;
+   esac
+done
+
+ffmpeg -hide_banner -framerate $rate -i "$input" -c:v libx264 -vf tinterlace=interleave_top,fieldorder=tff -flags +ildct+ilme -crf 0 -preset veryslow "$output"
