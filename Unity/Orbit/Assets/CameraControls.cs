e5e5313892bb495b838654b5a39bf9a8a7ba4608
fsce <5254212@student.gloscol.ac.uk>
2023-03-16 15:57:55 +0000

    orbit + 500 words on hitman paris

---
+++ Unity/Orbit/Assets/CameraControls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+        private void OnMouseDown() {
+            
+            
+
+
+        }
+
+
+
+
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ Unity/Orbit/Assets/Planet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : SimulatedObject
+{
+    public float gravitationalPull;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
